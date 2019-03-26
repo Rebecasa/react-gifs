7fb992087820496ae57c4614358ed183cf2363cd
Rebeca Casa <rebi_britto@hotmail.com>
2019-03-26 15:38:24 +0100

    first app working

---
+++ src/components/app.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId:"DQbNplcZ343hDntDM2"
+    }
+  }
+
+  search = (query) => {
+    giphy('1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm').search({
+        q: query,
+        rating: 'g',
+        limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+
+  }
+
+
+  render() {
+    return(
+      <div>
+        <div className="left-scene">
+          <SearchBar search={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId}/>
+          </div>
+        </div>
+
+        <div className="right-scene">
+        <GifList gifs={this.state.gifs} />
+        </div>
+      </div>
+      );
+  }
+}
+
+export default App;
